181619cda584fb4f40aaa864c37b107589a7cb93
Agus Suroyo <jony.extenz@gmail.com>
2026-07-10 13:06:44 +0700

    feat: map securityDefinitions to components.securitySchemes (#7)

--- swaggeropenapi.php
+++ swaggeropenapi.php
@@ -27,7 +27,35 @@ class Spec30Formatter implements SwaggerSpecFormatter {
 		$spec['servers'] = $this->mapServers( $spec );
 		unset( $spec['swagger'], $spec['host'], $spec['basePath'], $spec['schemes'] );
 
+		if ( isset( $spec['securityDefinitions'] ) ) {
+			$schemes = $this->mapSecuritySchemes( $spec['securityDefinitions'] );
+			if ( ! empty( $schemes ) ) {
+				$spec['components'] = array( 'securitySchemes' => $schemes );
+			}
+			unset( $spec['securityDefinitions'] );
+		}
+
 		return $spec;
+	}
+
+	private function mapSecuritySchemes(array $defs): array {
+		$out = array();
+		foreach ( $defs as $key => $def ) {
+			$type = isset( $def['type'] ) ? $def['type'] : '';
+			if ( 'basic' === $type ) {
+				$out[ $key ] = array( 'type' => 'http', 'scheme' => 'basic' );
+			} elseif ( 'bearer' === $key ) {
+				$scheme = array( 'type' => 'http', 'scheme' => 'bearer' );
+				if ( isset( $def['description'] ) ) {
+					$scheme['description'] = $def['description'];
+				}
+				$out[ $key ] = $scheme;
+			} else {
+				$out[ $key ] = $def; // already valid 3.0 (e.g. apiKey)
+			}
+		}
+
+		return $out;
 	}
 
 	private function mapServers(array $spec): array {
